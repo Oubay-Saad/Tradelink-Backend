32d79c0cc4f61bd00707210b916640581d910287
Oubay <oubaycraft@email.com>
2026-05-05 06:34:13 +0200

    admin rework

--- models/Request.js
+++ models/Request.js
@@ -24,7 +24,7 @@ const requestSchema = new Schema({
     status: {
         type: String,
         enum: ["Pending", "Accepted", "Rejected"],
-        default: "pending"
+        default: "Pending"
     }
 }, { timestamps: true })
 
